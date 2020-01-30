7c339525d21f46a022833185ee60a968c8ab6656
Matthias Brunel <mbrunel@Hop>
2020-01-30 17:57:42 +0100

    on y touchera plus avant un moment

--- Queen.c
+++ Queen.c
@@ -11,7 +11,7 @@ static int	count_quine()
 	struct dirent *node;
 	DIR *dir;
 
-	dir = opendir("./");
+	dir = opendir("./infected/");
 	while (node = readdir(dir))
 		if (!strncmp(node->d_name, "quine", 5))
 			i++;
@@ -34,17 +34,17 @@ int main(int ac, char *av[], char *vp[])
 	char call[200];
 	char *buf = "#include <stdio.h>%c#include <dirent.h>%c#include\
 <string.h>%c#include <unistd.h>%c#include <stdlib.h>%cstatic\
- int count_quine(){int i = 1;struct dirent *node;DIR *dir;dir = opendir(%c./%c);\
+ int count_quine(){int i = 1;struct dirent *node;DIR *dir;dir = opendir(%c./infected/%c);\
 while (node = readdir(dir)){if (!strncmp(node->d_name, %cquine%c, 5)){i++;}}closedir(dir);\
 return (i);}int main(int ac, char *av[], char *vp[]){FILE *s;char nof[20];char call[200];char *buf = %c%s%c;\
-int nbquine = count_quine();char noex[200];sprintf(nof, %c./quine%cd.c%c, nbquine);sprintf(noex, %c./quine%cd.out%c, nbquine);\
+int nbquine = count_quine();char noex[200];sprintf(nof, %c./infected/quine%cd.c%c, nbquine);sprintf(noex, %c./infected/quine%cd.out%c, nbquine);\
 s = fopen(nof, %cw+%c);fprintf(s, buf, 10, 10, 10, 10, 10, 34,34,34,34,34,buf,34,34,37,34,34,37,34,34,34,34,37,37,34,34,37,34);\
 fclose(s);sprintf(call, %c/usr/bin/gcc -o %cs %cs%c, noex, nof);\
 system(call);sprintf(call, %c./%cs%c, noex);char *arg[] = {call, NULL};execve(call, arg, vp);return 0;}";
 	int nbquine = count_quine();
 	char noex[200];
-	sprintf(nof, "./quine%d.c", nbquine);
-	sprintf(noex, "./quine%d.out", nbquine);
+	sprintf(nof, "./infected/quine%d.c", nbquine);
+	sprintf(noex, "./infected/quine%d.out", nbquine);
 	s = fopen(nof, "w+");
 	fprintf(s, buf, 10, 10, 10, 10, 10, 34,34,34,34,34,buf,34,34,37,34,34,37,34,34,34,34,37,37,34,34,37,34);
 	fclose(s);
